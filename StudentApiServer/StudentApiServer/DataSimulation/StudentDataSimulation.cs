63ca258438c32e18438e7ae9b5e09f6b21f093b3
Youssef Darrag <darrag904@gmail.com>
2026-03-22 23:26:55 +0200

    Initialize list of student object in memory.

---
+++ StudentApiServer/StudentApiServer/DataSimulation/StudentDataSimulation.cs
@@ -0,0 +1,41 @@
+﻿using StudentApiServer.Models;
+
+namespace StudentApiServer.DataSimulation
+{
+    public class StudentDataSimulation
+    {
+        // Static list of students, acting as an in-memory data store, I can change it later on to retrieve students from Database.
+        public static readonly List<Student> Students = new List<Student>
+        {
+            // Initialize the list with some student objects.
+            new Student
+            {
+                Id = 1,
+                Name = "Ali Ahmed",
+                Age = 20,
+                Grade = 88
+            },
+            new Student
+            {
+                Id = 2,
+                Name = "Fadi Mohammed",
+                Age = 22,
+                Grade = 77
+            },
+            new Student
+            {
+                Id = 3,
+                Name = "Ola Jaber",
+                Age = 21,
+                Grade = 66
+            },
+            new Student
+            {
+                Id = 4,
+                Name = "Alia Maher",
+                Age = 19,
+                Grade = 44
+            }
+        };
+    }
+}
