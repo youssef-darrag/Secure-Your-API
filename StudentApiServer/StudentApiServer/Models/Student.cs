e713cf350e7640ab6691f1210809b62ca4c7c8db
Youssef Darrag <darrag904@gmail.com>
2026-03-22 23:22:11 +0200

    Add student model.

---
+++ StudentApiServer/StudentApiServer/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentApiServer.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public int Age { get; set; }
+        public int Grade { get; set; }
+    }
+}
