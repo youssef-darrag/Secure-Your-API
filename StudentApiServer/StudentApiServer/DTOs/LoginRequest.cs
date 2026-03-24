971b011f984f33eb0ea613dd343cf92b2b74c34a
Youssef Darrag <darrag904@gmail.com>
2026-03-24 23:37:09 +0200

    Add login request dto.

---
+++ StudentApiServer/StudentApiServer/DTOs/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentApiServer.DTOs
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
